6eb090e30d804ec72e377ebda94ee89dcea735cb
losowy <arandomone.businnes@gmail.com>
2022-02-15 14:48:09 +0100

    pretty cool changes

--- index.js
+++ index.js
@@ -1,41 +1,32 @@
-let val = 0
+var i = 0
+var txt = 'Welcome' /* The text */
+var speed = 50 /* The speed/duration of the effect in milliseconds */
 
-document.addEventListener("keydown", e => {
+if (i < txt.length) {
+	document.getElementById('tw').innerHTML += txt.charAt(i)
+	i++
+	setTimeout(typeWriter, speed)
+}
+
+document.addEventListener('keydown', (e) => {
 	switch (e.key.toLowerCase()) {
-		case "1":
-			window.open("https://beatsaver.com", "_top")
+		case '1':
+			window.open('https://beatsaver.com', '_top')
 			break
-		case "2":
-			window.open("https://github.com", "_top")
+		case '2':
+			window.open('https://github.com', '_top')
 			break
-		case "3":
-			window.open("https://mail.google.com", "_top")
+		case '3':
+			window.open('https://mail.google.com', '_top')
 			break
-		case "4":
-			window.open("https://drive.google.com", "_top")
+		case '4':
+			window.open('https://drive.google.com', '_top')
 			break
-		case "5":
-			window.open("https://youtube.com", "_top")
+		case '5':
+			window.open('https://youtube.com', '_top')
 			break
-		case "6":
-			window.open("https://wakatime.com/dashboard", "_top")
-			break
-	}
-})
-
-document.addEventListener("wheel", e => {
-	val = e.deltaY
-
-	switch (e.deltaY) {
-		case 100:
-			document.body.style.background = "#141414"
-			break
-		case 200:
-			document.body.style.background = "yellow"
-			break
-		case 300:
-			document.body.style.background = "red"
+		case '6':
+			window.open('https://wakatime.com/dashboard', '_top')
 			break
 	}
 })
-
