62eb58bce52ce7f863dd2a7107acfd42e230cc5a
losowy <arandomone.businnes@gmail.com>
2022-02-16 18:06:41 +0100

    move all js to the js file and stuff

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
-key = 'e6e4525b8870b3f08c362da4c1d63f32'
-city = 'sandnes'
+//#region typewriter
+var i = 0
+var txt = 'Welcome.'
+var speed = 70
+
+function typeWriter() {
+	if (i < txt.length) {
+		document.getElementById('tw').innerText += txt.charAt(i)
+		i++
+		setTimeout(typeWriter, speed)
+	}
+}
+
+typeWriter()
+//#endregion
+
+//#region weather data
+key = 'fuck you lol'
+city = 'also fuck you'
 
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`)
 	.then((res) => res.json())
 	.then((data) => {
-		console.log(data)
 		document.getElementById(
 			'weather'
 		).innerText = `${data.weather[0].main}, ${data.main.temp}°C`
 	})
+//#endregion
 
+//#region clock
+function clock() {
+	const date = new Date()
+
+	document.getElementById('time').innerText = date.toLocaleTimeString()
+}
+
+setInterval(clock, 100)
+//#endregion
+
+//#region date
+const date = new Date()
+
+const day = date.getDate()
+const year = date.getFullYear()
+const month = date.getMonth()
+
+document.getElementById('date').innerText = `${day}/${month}/${year}`
+//#endregion
+
+//#region keyboard shortcuts
 document.addEventListener('keydown', (e) => {
 	switch (e.key.toLowerCase()) {
 		case '1':
@@ -32,3 +70,4 @@ document.addEventListener('keydown', (e) => {
 			break
 	}
 })
+//#endregion
